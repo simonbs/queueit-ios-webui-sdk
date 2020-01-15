e6feef061f9fde09ef8a0b7869aa772f2a7b566a
Simon Støvring <mail@simonbs.dk>
2020-01-15 15:46:20 +0100

    Adds QueueITKit target

---
+++ QueueITKit/QueueITKit.h
@@ -0,0 +1,20 @@
+//
+//  QueueITKit.h
+//  QueueITKit
+//
+//  Created by Simon Støvring on 15/01/2020.
+//  Copyright © 2020 Qeueuit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QueueITKit.
+FOUNDATION_EXPORT double QueueITKitVersionNumber;
+
+//! Project version string for QueueITKit.
+FOUNDATION_EXPORT const unsigned char QueueITKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QueueITKit/PublicHeader.h>
+
+
+#import <QueueITKit/QueueITEngine.h>
